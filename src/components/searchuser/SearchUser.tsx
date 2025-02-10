291df556ae85eaaba73633872dcc8384b3d74266
Rahul917-Kumar <rahul66.ugcs20@iiitranchi.ac.in>
2025-02-10 20:21:58 +0530

    shift searchUser css to searchUser-css

--- src/components/searchuser/SearchUser.tsx
+++ src/components/searchuser/SearchUser.tsx
@@ -10,6 +10,7 @@ import UserList from "../userlist/UserList";
 import UserCardShimmer from "../shimmer/UserCardShimmer"
 import { cn } from "@/lib/utils"
 import NotFound from "../notfound/NotFound";
+import { searchUserCss } from "./searchUser-css";
 
 const apiEndPoint = import.meta.env.VITE_API_API_ENDPOINT;
 
@@ -56,48 +57,33 @@ function SearchUser() {
     }
     return (
         <div>
-            <div 
-                className={`
-                    flex md:flex-col absolute md:top-[260px] md:left-[560px] ${!isSearch ?"gap-[80px]":""} 
-                    left-[40px]
-                `}
-            >
+            <div className={`${searchUserCss.container} ${!isSearch ? "gap-[80px]" : ""}`}>
                 {
                     isSearch?(
                         <></>
                     ):(
                         <>        
-                            <div 
-                                className={`hidden md:flex md:w-[800px] md:h-[125px] md:gap-[39.99px] transition-all duration-5000 ease-in-out ${isSearch ? "opacity-0 -translate-y-5 pointer-events-none" : "opacity-100 translate-y-0"
-                                    }`}
-                                >
+                            <div className={searchUserCss.girmanSearchImageContainer}>
                                 <img
                                     src={GirmanLogo}
                                     alt="Girman Logo"
-                                    className="md:w-[187.31px] md:h-[125px]"
+                                    className={searchUserCss.girmanSearchImageLogo}
                                 ></img>
                                 <img
                                     src={GirmanLogoName}
                                     alt="Girman Logo Name"
-                                    className="md:w-[572.7px] md:h-[125px]"
+                                    className={searchUserCss.girmanSearchImage}
                                 ></img>
                             </div>
                         </>
                     )
                 }
-                <div
-                    className="flex flex-col md:top-[260px] md:left-[590px] gap-[40px] items-center justify-center"
-                >
-                    <div 
-                        className="
-                            relative md:w-[800px] md:h-[50px] md:rounded-xl md:gap-4 md:mt-[0px] pl-4 pr-4 border border-[#D7D7EA] bg-white shadow-[0_4px_10px_0px_#0000000D]
-                            w-[313px] h-[40px] gap-[16px] rounded-[12px] border border-[1px] 
-                            mt-[40px]
-                        ">
-                        <Search className="absolute left-4 top-1/2 transform -translate-y-1/2 md:h-[16px] md:w-[16px] text-gray-500 h-[14px] w-[14px]" />
+                <div className={searchUserCss.inputListParentContainer}>
+                    <div className={searchUserCss.inputParentContainer}>
+                        <Search  className={searchUserCss.searchIcon}/>
                         <Input
                             placeholder="Search"
-                            className="w-full md:pl-[32px] pl-[30px] placeholder:[font-family:'Sans'] placeholder:font-medium font-[Sans] text-gray-500 font-medium caret-gray-500 placeholder:text-[16px] md:placeholder:text-[18px] text-[16px] md:text-[18px] h-full border-none shadow-none !outline-none !ring-0 !border-transparent"
+                            className={searchUserCss.input}
                             value={searchQuery}
                             onChange={(e) => setSearchQuery(e.target.value)}
                             onKeyDown={handlePressEnter}
