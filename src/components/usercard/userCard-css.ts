3dc2e9a59a5e218819fa22b2a5a52d9e473d23dd
Rahul917-Kumar <rahul66.ugcs20@iiitranchi.ac.in>
2025-02-10 21:26:48 +0530

    shift userCard css to userCard-css

---
+++ src/components/usercard/userCard-css.ts
@@ -0,0 +1,22 @@
+export const userCardCss = {
+    "parentContainer":"md:w-[388.57px] md:h-[312.57px] md:rounded-[18.29px] md:p-[27.43px] bg-white w-[313px] h-[236.21px] gap-[8.65px] rounded-[13.84px] border border-[#E3E3E3] p-[20.75px]",
+    "parentFirstChild":"flex flex-col md:w-[333.71px] md:h-[257.71px] md:gap-[36.57px] w-[252.49px] h-[194.7px] gap-[27.67px]",
+    "userImageNameContainer":"flex flex-col md:h-[166.29px] md:gap-[9.14px] h-[125.52px] gap-[6.92px]",
+    "profileContainer":"md:w-[89.14px] md:h-[89.14px] rounded-full border md:border-[1.14px] border-[#F3F3F3] flex justify-center items-center w-[67.45px] h-[67.45px] border-[0.86px]",
+    "profileImage":"md:w-[70.86px] md:h-[70.86px] rounded-full w-[53.61px] h-[53.61px]",
+    "userNameContainer":"flex flex-col  md:h-[68px] md:gap-[9.14px] h-[51.16px] gap-[6.92px]",
+    "userName":"md:h-[44px] font-inter font-semibold md:text-[36.57px] md:leading-[44.26px] tracking-normal h-[33px] text-[27.67px] leading-[33.49px]",
+    "locationContainer":"flex items-center md:gap-[9.14px] md:h-[14.86px] h-[11.24px] gap-[6.92px]",
+    "locationIcon":"md:w-[11.43px] md:h-[14.86px] w-[8.65px] h-[11.24px]",
+    "cityDetail":"md:w-[44px] md:h-[14px] font-inter font-medium md:text-[11.43px] md:leading-[13.83px] tracking-normal text-[#425763] w-[34px] h-[10px] text-[8.65px] leading-[10.46px]",
+    "userPhoneFetchDetailParentContainer":"flex flex-col md:h-[54.86px] md:gap-[9.14px] h-[41.51px] gap-[6.92px]",
+    "userPhoneFetchDetailChild":"flex items-center md:h-[45.71px] md:gap-[81.14px] h-[34.59px] gap-[61.39px]",
+    "phoneParentContainer":"flex flex-col md:w-[115.43px] md:h-[36.71px] md:gap-[5.71px] w-[87.34px] h-[27.32px] gap-[4.32px]",
+    "phoneContainer":"flex flex-row md:h-[17px] md:gap-[6.86px] h-[13px] gap-[5.19px]",
+    "phoneIcon":"md:h-[14.86px] md:w-[14.86px] h-[11.24px] w-[11.24px]",
+    "contactDetails":"md:w-[93px] md:h-[17px] font-inter font-semibold md:text-[13.71px] md:leading-[16.6px] tracking-normal w-[71px] h-[13px] text-[10.38px] leading-[12.56px]",
+    "availableOnPhone":"md:h-[14px] font-inter font-medium md:text-[11.43px] md:leading-[13.83px] tracking-normal text-[#AFAFAF] h-[10px] text-[8.65px] leading-[18.46px]",
+    "fetchDetailsContainer":"flex items-center justify-center md:w-[136.57px] md:h-[45.71px] md:min-h-[45.71px] md:max-h-[45.71px] md:gap-[11.43px] md:rounded-[9.14px] pl-[4px] pr-[4px] pt-[2px] pb-[2px] !bg-[#18181B] w-[103.67px] h-[34.59px] min-h-[34.59px] max-h-[34.59px] gap-[8.65px] rounded-[6.92px]",
+    "fetchDetailsChildContainer":"flex flex-col md:w-[100px] md:h-[19px] md:gap-[11.43px] w-[76px] h-[15px] gap-[8.65px]",
+    "fetchDetailText":"font-inter font-medium md:text-base md:leading-[19.36px] tracking-normal text-[#FAFAFA] text-[12.11px] leading-[14.65px]"
+}
